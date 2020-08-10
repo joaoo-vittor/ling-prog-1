1529961b37c4e263146e0ffef74ad09198a7145b
João Vitor Silva <56928594+joaoo-vittor@users.noreply.github.com>
2020-08-10 00:46:44 -0300

    Add files via upload

---
+++ execicio_capitulo_3/3-27.c
@@ -0,0 +1,43 @@
+/*
+
+3.27 Ache os dois maiores números. Usando uma técnica
+semelhante à do Exercício 3.24, ache os dois maiores
+valores dos 10 números. [Nota: você poderá informar
+cada número apenas uma vez.]
+
+
+*/
+
+#include <stdio.h>
+
+int main() {
+  int bigger_1 = 0, bigger_2 = 0;
+  int number;
+
+  for (int i = 1; i <= 5; i++) {
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if(number > bigger_1){
+      if(bigger_1 > bigger_2){
+        bigger_2 = bigger_1;
+      }
+
+      bigger_1 = number;
+    }
+
+    if(bigger_1 >= bigger_2 && number != bigger_1){
+      if(number > bigger_2){
+        bigger_2 = number;
+      }
+    }
+
+  }
+
+  printf("The first bigger number is %d\n", bigger_1);
+  printf("The second bigger number is %d", bigger_2);
+
+
+  return 0;
+}
